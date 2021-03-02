067fdf65a23fcae5dad43243176fcf3a2827f9dd
tphilippini <thomas.philippini@gmail.com>
2021-03-02 18:53:00 +0100

    Initial commit

---
+++ screens/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <StatusBar style='light' />
+      <Image
+        source={require("../assets/images/logo.png")}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder='Email'
+          autoFocus
+          type='email'
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder='Password'
+          secureTextEntry
+          type='password'
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title='Login' />
+      <Button
+        containerStyle={styles.button}
+        onPress={() => navigation.navigate("Register")}
+        type='outline'
+        title='Register'
+      />
+      <View style={{ height: 200 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 300,
+    marginTop: 10,
+  },
+});
---
+++ screens/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Register = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Hello Register</Text>
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({});
